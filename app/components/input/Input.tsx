0190a7bdcbadc348092aeb8b4752b89b87957d8e
YIFAN MA <61387216+Yairma97@users.noreply.github.com>
2023-11-18 00:15:57 +0800

    完成登录页设计

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -4,7 +4,7 @@ import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 interface InputProps {
   label: string;
   id: string;
-  type: string;
+  type?: string;
   required?: boolean;
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
@@ -23,8 +23,7 @@ const Input: React.FC<InputProps> = ({
     <div>
       <label
         className="block text-sm font-medium leading-6 text-gray-900"
-        htmlFor={id}
-      >
+        htmlFor={id}>
         {label}
       </label>
       <div className="mt-2">
@@ -34,7 +33,29 @@ const Input: React.FC<InputProps> = ({
           autoComplete={id}
           disabled={disabled}
           {...register(id, { required: required })}
-        ></input>
+          className={clsx(
+            `
+           form-input
+           block
+           w-full
+           rounded-md
+           border-0
+           py-1.5
+           text-gray-900
+           shadow-sm
+           ring-1
+           ring-inset
+           ring-gray-300
+           placeholder:text-gray-400
+           focus:ring-2
+           focus:ring-inset
+           focus:ring-sky-50
+           sm:text-sm
+           sm:leading-6
+           `,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}></input>
       </div>
     </div>
   );
