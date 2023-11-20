3f24b3c870b3a2447179020303bd2220edbc9bbd
mayifan <mayifan@lrhealth.com>
2023-11-20 18:10:26 +0800

    axios

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
 
 type Variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
@@ -35,6 +36,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
+      axios.post("/api/register",data)
     }
     if (variant === "LOGIN") {
     }
